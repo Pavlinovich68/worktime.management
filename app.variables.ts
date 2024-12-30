e45ece0f21fac998e5d5b029f8b3569d1f9edea7
Pavlinovich68 <54423661+Pavlinovich68@users.noreply.github.com>
2024-12-30 09:52:58 +0500

    !

--- app.variables.ts
+++ app.variables.ts
@@ -1,5 +1,5 @@
 export const $app_variables = {
    URL: 'http://localhost:3000',
-   TITLE: "Управление проектами",
+   TITLE: "Управление рабочим временем",
    APP_NAME: "WorkTime.Management"
 };
